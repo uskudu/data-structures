afb00b984ed3660fc157a05bdca4292e00cbfbbc
uskudu <usskudu@gmail.com>
2026-03-17 18:39:01 +0300

    sll push front

--- linked-lists/singly-linked-list/sll.go
+++ linked-lists/singly-linked-list/sll.go
@@ -1,1 +1,22 @@
 package singly_linked_list
+
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+type LinkedList struct {
+	Head Node
+}
+
+func NewLinkedList(head Node) *LinkedList {
+	return &LinkedList{Head: head}
+}
+
+func (l *LinkedList) PushFront(val int) {
+	n := Node{Value: val}
+
+	prev := l.Head
+	n.Next = &prev
+	l.Head = n
+}
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	sll "awesomeDataStructures/linked-lists/singly-linked-list"
+	"fmt"
+)
+
 func main() {
+	n := sll.Node{
+		Value: 1,
+		Next:  nil,
+	}
+
+	l := sll.NewLinkedList(n)
+
+	fmt.Println(l.Head)
+
+	l.PushFront(2)
+	fmt.Println(l.Head)
 
 }
