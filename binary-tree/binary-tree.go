5505c70f16923c04e2d0890e409fd8e7cf9e176f
uskudu <usskudu@gmail.com>
2026-03-22 15:04:46 +0300

    bt sliced

--- binary-tree/binary-tree.go
+++ binary-tree/binary-tree.go
@@ -13,6 +13,37 @@ func NewBinaryTree(rootNodeValue int) *binaryTree {
 	return &binaryTree{t}
 }
 
+func (t *binaryTree) Sliced() [][]int {
+	if t.root == nil {
+		return nil
+	}
+	var st [][]int
+	queue := []*node{t.root}
+
+	for len(queue) > 0 {
+		levelSize := len(queue)
+		var level []int
+
+		for i := 0; i < levelSize; i++ {
+			n := queue[0]
+			queue = queue[1:]
+
+			level = append(level, n.val)
+
+			if n.left != nil {
+				queue = append(queue, n.left)
+			}
+			if n.right != nil {
+				queue = append(queue, n.right)
+			}
+		}
+
+		st = append(st, level)
+	}
+
+	return st
+}
+
 func (t *binaryTree) Insert(val int) {
 	n := &node{
 		val: val,
@@ -26,7 +57,7 @@ func (t *binaryTree) Insert(val int) {
 	var inner func(cur *node)
 
 	inner = func(cur *node) {
-		if val > cur.val {
+		if val < cur.val {
 			if cur.left == nil {
 				cur.left = n
 				return
@@ -37,7 +68,7 @@ func (t *binaryTree) Insert(val int) {
 				cur.right = n
 				return
 			}
-			inner(cur.left)
+			inner(cur.right)
 		}
 	}
 	inner(t.root)
